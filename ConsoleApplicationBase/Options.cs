8b62594da8489cc0ba5c84c5f933667e4bc49c83
RubDel <RubenDelange@users.noreply.github.com>
2014-09-24 10:57:16 +0200

    first checkin

---
+++ ConsoleApplicationBase/Options.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace ConsoleApplicationBase
+{
+    public class Options
+    {
+        [Option('i', "input", Required = false, HelpText = "Input file to read commands in batch.")]
+        public string InputFile { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            var help = new HelpText
+                            {
+                                Heading = new HeadingInfo("Console Application Base", "1.0"),
+                                Copyright = new CopyrightInfo("Ruben Delange", 2014),
+                                AdditionalNewLineAfterOption = true,
+                                AddDashesToOption = true
+                            };
+
+            HandleParsingErrorsInHelp(help);
+
+            help.AddPreOptionsLine("Usage: ConsoleApplicationBase [-i batchCommands.txt]");
+            help.AddOptions(this);
+
+            return help;
+        }
+
+        private void HandleParsingErrorsInHelp(HelpText help)
+        {
+            string errors = help.RenderParsingErrorsText(this, 2);
+            if (!string.IsNullOrEmpty(errors))
+            {
+                help.AddPreOptionsLine(string.Concat(Environment.NewLine, "ERROR: ", errors, Environment.NewLine));
+            }
+        }
+
+    }
+}
